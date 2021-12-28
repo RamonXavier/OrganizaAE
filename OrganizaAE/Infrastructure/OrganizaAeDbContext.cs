6be04e75c2d36c47bbc075c462b50fffbe01e1a1
GTIT\ramon.nascimento <ramon.nascimento@guiandotelecom.com.br>
2021-12-27 23:29:18 -0300

    Add project files.

---
+++ OrganizaAE/Infrastructure/OrganizaAeDbContext.cs
@@ -0,0 +1,17 @@
+﻿
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace OrganizaAE.Infrastructure
+{
+    public class OrganizaAeDbContext : IdentityDbContext<IdentityUser>
+    {
+        public OrganizaAeDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+
+    }
+}
